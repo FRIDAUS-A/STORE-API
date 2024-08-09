962c07508125a15bbf640b973cc651faf435fee3
FRIDAUS <ayobamifridaus@gmail.com>
2024-08-09 02:57:39 +0100

    update

---
+++ controllers/products.js
@@ -0,0 +1,65 @@
+const { queryObjects } = require('v8')
+const Product = require('../models/Product')
+
+const getAllProductsStatic = async (req, res) => {
+	//const search = 'aa'
+	const products = await Product.find({ price: {$gt: 30} }).sort('name').select('name price').limit(4).skip(1)
+	res.status(200).json({nbHits: products.length, products: products})
+}
+
+const getAllProducts = async (req, res) => {
+	const { featured, company, name, sort, fields, numericFilters } = req.query
+	const queryObject = {} 
+	if (featured) {
+		queryObjects.featured = featured === 'true'? true : false
+	}
+	if (company) {
+		queryObject.company = company
+	}
+	if (name) {
+		queryObject.name = { $regex: name, $options: 'i'}
+	}
+	let result = Product.find(queryObject)
+	if (sort) {
+		const sortList = sort.split(',').join(' ')
+		console.log(sort)
+		result = result.sort(sortList)
+		//products = result.sort()
+	}
+	else {
+		result = result.sort('createdAt')
+	}
+	if (fields) {
+		const fieldsList = fields.split(',').join(' ')
+		result = result.select(fieldsList)
+	}
+
+	const page = Number(req.query.page) || 1
+	const limit = Number(req.query.limit) || 10
+	const skip = (page - 1) * limit
+	result = result.skip(skip).limit(limit)
+
+	if (numericFilters) {
+		const operatorMap = {
+			'>': '$gt',
+			'>=': '$gte',
+			'=':  '$eq',
+			'<':  '$lt',
+			'<=': '$lte'
+		}
+		const regEx = /\b(<|>|>=|=|<|<=)\b/g
+		let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`)
+		console.log(filters)
+		const options = ['price', 'rating']
+		filters = filters.split(',').forEach((item) => {
+			const [field,operator,value] = item.split('-')
+			if (options.includes(field)) {
+				queryObject[field] = { [operator]: Number(value) }
+			}
+		})
+		//console.log(queryObject)
+	}
+	const products = await result.find(queryObject)
+	res.status(200).json({products, length: products.length})
+}
+module.exports ={getAllProductsStatic, getAllProducts}
---
+++ db/connect.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const connectDB = (url) => {
+	return mongoose.connect(url, {
+		useNewUrlParser: true,
+		useUnifiedTopology:true,
+		useCreateIndex: true,
+    	useFindAndModify: false
+	}).then(() => console.log('CONNECTED TO DB...')).catch((err) => console.log(err))
+}
+
+module.exports = connectDB
