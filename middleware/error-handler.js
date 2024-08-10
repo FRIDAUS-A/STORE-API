eb130863ccf95eab0beedbca0a333f1d1a303869
FRIDAUS <ayobamifridaus@gmail.com>
2024-08-10 10:44:51 +0100

    syntax corretced

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,7 @@
 CustomAPIError = require('../errors/custom-error')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-	if (err instance of CustomAPIError) {
+	if (err instanceof CustomAPIError) {
 		res.status(err.statusCode).json({message: err.message})
 	} else {
 		res.status(500).json({message: err.message})
